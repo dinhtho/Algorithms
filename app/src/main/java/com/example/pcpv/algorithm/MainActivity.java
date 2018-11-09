ea31a457282415b079ea86c2f9aa262b715aabab
thonguyen <dinhthohcmus@gmail.com>
2018-11-09 17:29:46 +0700

    first commit

---
+++ app/src/main/java/com/example/pcpv/algorithm/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.pcpv.algorithm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int arr[] = {0, 15, 20, 35, 40, 54, 62, 77, 84};
+        Log.i(TAG, "onCreateRecursion : " + binarySearchRecursion(arr, 0));
+        Log.i(TAG, "onCreate: " + binarySearch(arr, 0));
+    }
+
+    private static final String TAG = "MainActivity";
+
+    private static Integer binarySearchRecursion(int arr[], int number) {
+        int start = 0;
+        int end = arr.length;
+        int middle = (end - start) / 2;
+        if (end > start) {
+            if (arr[middle] > number) {
+                end = middle;
+            } else if (arr[middle] < number) {
+                start = middle + 1;
+            } else {
+                return start + middle;
+            }
+            return binarySearchRecursion(Arrays.copyOfRange(arr, start, end), number);
+        }
+        return null;
+    }
+
+    private static Integer binarySearch(int arr[], int number) {
+        int start = 0;
+        int end = arr.length;
+        while (end > start) {
+            int middle = start + (end - start) / 2;
+            if (arr[middle] > number) {
+                end = middle - 1;
+            } else if (arr[middle] < number) {
+                start = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return null;
+    }
+}
